6dd000bb16b9da404eda6d8870a18fafdd617d97
Silundan <admin@127.0.0.1>
2023-09-07 18:32:25 +0930

    updated the sql, and adding specific folder for it

---
+++ _DATABASE_/db_rebuild_070923.sql
@@ -0,0 +1,139 @@
+CREATE DATABASE  IF NOT EXISTS `adelaide` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `adelaide`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: adelaide
+-- ------------------------------------------------------
+-- Server version	8.0.34-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course` (
+  `subject_area` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `course_code` varchar(255) NOT NULL,
+  `course_name` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `courselink_href` varchar(255) DEFAULT NULL,
+  `pre_requisite` varchar(255) DEFAULT NULL,
+  `Incompatibale` varchar(255) DEFAULT NULL,
+  `belongs_to` varchar(255) DEFAULT NULL,
+  `units` int DEFAULT NULL,
+  `term` varchar(45) DEFAULT NULL,
+  `career` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`course_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES ('Unknown_course_sub-area','2000','COMP SCI 2000','Computer System','Computer System','http://course-outline/.....','One of COMP SCI 7777, COMPSCI 7777','COMP SCI 7777, COMPSCI 7777\"','B-Computer Science-core',3,'1','Unknown_course_career');
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `degree`
+--
+
+DROP TABLE IF EXISTS `degree`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `degree` (
+  `degree` varchar(255) NOT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `streams` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`degree`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `degree`
+--
+
+LOCK TABLES `degree` WRITE;
+/*!40000 ALTER TABLE `degree` DISABLE KEYS */;
+INSERT INTO `degree` VALUES ('Master of Computer Science','Unknown_dg_content','Core');
+/*!40000 ALTER TABLE `degree` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `degree_course`
+--
+
+DROP TABLE IF EXISTS `degree_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `degree_course` (
+  `degree` varchar(255) NOT NULL,
+  `stream` varchar(255) DEFAULT NULL,
+  `supplement` varchar(255) DEFAULT NULL,
+  `courses` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`degree`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `degree_course`
+--
+
+LOCK TABLES `degree_course` WRITE;
+/*!40000 ALTER TABLE `degree_course` DISABLE KEYS */;
+INSERT INTO `degree_course` VALUES ('Master of Computer Science','Core','Unknown_dg-course_sup','COMP SCI 2000','Unknown_dg-course_name');
+/*!40000 ALTER TABLE `degree_course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `degree_streams`
+--
+
+DROP TABLE IF EXISTS `degree_streams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `degree_streams` (
+  `degree` varchar(255) NOT NULL,
+  `streams` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`degree`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `degree_streams`
+--
+
+LOCK TABLES `degree_streams` WRITE;
+/*!40000 ALTER TABLE `degree_streams` DISABLE KEYS */;
+INSERT INTO `degree_streams` VALUES ('Unknown_dg-str_degree','Unknown_dg-str_streams');
+/*!40000 ALTER TABLE `degree_streams` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-07 18:25:24
