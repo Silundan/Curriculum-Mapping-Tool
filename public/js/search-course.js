1d52df7f73ca5775a432057b6d5b2fc15fcc12fa
mzh-23 <76993342+mzh-23@users.noreply.github.com>
2023-09-10 15:01:52 +0930

    Update search-course.js

--- public/js/search-course.js
+++ public/js/search-course.js
@@ -1,5 +1,17 @@
-function search_course(){
-    let searched_course = document.getElementById('search-bar').value;
-    console.log(searched_course);
-    window.open(`/search?all-categories=${searched_course}`, '_blank').focus();
+function search_course() {
+    let searched_course = document.getElementById('search-bar').value.trim();
+
+    
+    if (!searched_course) {
+        alert('search here');
+        return;
+    }
+
+    
+    const params = new URLSearchParams();
+    params.append('all-categories', searched_course);
+
+    const url = `/search?${params.toString()}`;
+    
+    window.open(url, '_blank').focus();
 }
